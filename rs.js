08c1602c237f5e6404fd5ecea795a0472b67fb09
Johan <johandp92@gmail.com>
2016-05-14 23:53:59 +0200

    Switched to multi-canvas animation
    - Removed main use of setInterval
    - Switched to a multi-canvas setup
    - Implemented use of requestAnimationFrame

--- rs.js
+++ rs.js
@@ -1,80 +1,95 @@
-var canvas = document.getElementById('myCanvas');
-var c = canvas.getContext("2d");
-var rectangles = [];
-var animation;
-var circle = {
-	x: Math.random() * canvas.width,
-	y: Math.random() * canvas.height,
-	r: 1
-};
-var genRect = {
-	x: 0,
-	y: 0,
-	w: 50,
-	h: 50
-};
+// Setting canvas and context variables that will be used
+var canvasGrid = document.getElementById('grid');
+var canvasRipple = document.getElementById('ripple');
+var canvasBlocks = document.getElementById('blocks');
+var contextGrid = canvasGrid.getContext("2d");
+var contextRipple = canvasRipple.getContext('2d');
+var contextBlocks = canvasBlocks.getContext('2d');
+// Canvas Width and Height
+var canvasWidth = canvasGrid.width;
+var canvasHeight = canvasGrid.height;
+// Grid array that will store all the grid positions
+var grid = [];
+// Defaults
+var radius = 0;
+var rectanglePosX = 0;
+var rectanglePosY = 0;
+var requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame || 
+                            window.webkitRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
 
-var rectDraw = setInterval(function() {
-	c.strokeRect(genRect.x,genRect.y,genRect.w,genRect.h);
-	// Pushing rectangle object to array
-	rectangles.push({
-		x: genRect.x,
-		y: genRect.y,
-		w: genRect.w,
-		h: genRect.h
-	});
-	genRect.x = genRect.x + 50;
-	if (genRect.x > canvas.width) {
-		if (genRect.y > canvas.height) {
-			clearInterval(rectDraw);
-			setInterval(ripple, 100);
-		}
-		genRect.x = 0;
-		genRect.y = genRect.y + 50;
-	}
+function drawGrid() {
+	var complete = false;
 
-	// c.save();
+	contextGrid.strokeStyle="#EEEEEE";
+	contextGrid.strokeRect(rectanglePosX,rectanglePosY,20,20);
 
-}, 1);
-
-function ripple() {
-	if (circle.r > canvas.width + 400) {
-		clearInterval(animation);
-		circle = {
-			x: Math.random() * canvas.width,
-			y: Math.random() * canvas.height,
-			r: 1
-		}
-		c.clearRect(0,0,canvas.width,canvas.height);
-		// c.restore();
-	}
-	draw(circle.x, circle.y, circle.r);
-	$.each(rectangles, function(key, rect) {
-		collisionCheck(rect, circle);
+	// Populate the grid array
+	grid.push({
+		x: rectanglePosX,
+		y: rectanglePosY,
+		w: 20,
+		h: 20
 	});
 
+	rectanglePosX = rectanglePosX + 20;
+	if (rectanglePosX > canvasWidth) {
+		if (rectanglePosY > canvasHeight) {
+			complete = true;
+			drawRipple();
+		}
+		rectanglePosX = 0;
+		rectanglePosY = rectanglePosY + 20;
+	}
 
-	// c.clearRect(0,rect.y,rect.w,rect.h);
-	// c.strokeRect(rect.x,rect.y,rect.w,rect.h);
-};
+	if (!complete) {
+		requestAnimationFrame(drawGrid);
+	}
+}
 
-function collisionCheck(rect, circle) {
-	var rcx = Math.pow(Math.floor(rect.x) - Math.floor(circle.x),2);
-	var rcy = Math.pow(Math.floor(rect.y) - Math.floor(circle.y),2);
+function drawRipple() {
+    contextRipple.clearRect(0, 0, canvasWidth, canvasHeight);
+     
+    // draw the Ripple
+    contextRipple.beginPath();
+     
+    // var radius = 25 + 150 * Math.abs(Math.cos(angle));
+    contextRipple.arc(225, 225, radius, 0, Math.PI * 2, false);
+    contextRipple.closePath();
+
+    // Making the ripple invisible before we stroke
+    contextRipple.strokeStyle = "rgba(0,0,0,0.0)";
+     
+    // draw the Ripple
+    contextRipple.stroke();
+
+    // Run the collisions check
+    $.each(grid, function(key, rect) {
+    	collisionCheck(rect);
+    });
+
+    radius += 1;
+
+    if (radius > canvasWidth || radius > canvasHeight) {
+    	radius = 0;
+    	contextBlocks.clearRect(0,0,canvasWidth,canvasHeight);
+    }
+
+    requestAnimationFrame(drawRipple);
+}
+
+function collisionCheck(rect) {
+	// Need to add dynamic circle position here for X and Y 
+	var rcx = Math.pow(Math.floor(rect.x) - Math.floor(225),2);
+	var rcy = Math.pow(Math.floor(rect.y) - Math.floor(225),2);
 	var dist = Math.sqrt(rcx + rcy);
 
 	//  && circle.r - dist < 100
-	if (circle.r > dist && Math.random() > 0.8) {
-		c.fillStyle = "#D3D3D3";
-		c.fillRect(rect.x,rect.y,rect.w,rect.h);
+	if (radius > dist && Math.random() > 0.8) {
+		contextBlocks.fillStyle = "#D3D3D3";
+		contextBlocks.fillRect(rect.x,rect.y,rect.w,rect.h);
 	}
 }
 
-function draw(x, y, r) {
-	// c.globalAlpha = 0;
-	c.beginPath();
-	c.arc(x,y,r,0,2*Math.PI);
-	c.stroke();
-
-	circle.r = r + 20;
-}
+drawGrid();
